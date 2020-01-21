d6ba1b3073c3bf7a8c0f2943f9b91f7e194bea80
grudinsky <g.rudzianskas@gmail.com>
2020-01-21 11:51:17 +0000

    button and useState hook added

--- App.js
+++ App.js
@@ -1,10 +1,26 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React, { useState } from 'react'
+import { StyleSheet, Text, View, Button } from 'react-native'
 
 export default function App() {
+  const [name, setName] = useState('giedrius')
+  const [title, setTitle] = useState({ type: 'Android', content: 'dating' })
+  const handleClick = () => {
+    setName('grudinsky')
+    setTitle({ type: 'iOS', content: 'mapping' })
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Hello Worldotron</Text>
+      <View style={styles.header}>
+        <Text style={styles.boldText}>React Native App</Text>
+      </View>
+      <View style={styles.body}>
+        <Text style={styles.boldText}>Created by {name}</Text>
+        <Text>This is an {title.type} app for {title.content}.</Text>
+        <View style={styles.buttonContainer}>
+          <Button title='update state' onPress={handleClick} />
+        </View>
+      </View> 
     </View>
   )
 }
@@ -15,5 +31,19 @@ const styles = StyleSheet.create({
     backgroundColor: '#ddd',
     alignItems: 'center',
     justifyContent: 'center'
+  },
+  header: {
+    backgroundColor: 'pink',
+    padding: 20
+  },
+  boldText: {
+    fontWeight: 'bold'
+  }, 
+  body: {
+    backgroundColor: 'yellow',
+    padding: 20
+  }, 
+  buttonContainer: {
+    marginTop: 20
   }
 })
